f1fbb8ba06d70e558104bf2445db2f61b7691433
Lex Li <support@lextm.com>
2017-06-12 14:41:12 -0400

    Reverted the changes to entry.

--- JexusManager/Program.cs
+++ JexusManager/Program.cs
@@ -7,74 +7,23 @@ using Microsoft.Web.Administration;
 namespace JexusManager
 {
     using System;
-    using System.Diagnostics;
-    using System.Linq;
-    using System.Security.Principal;
     using System.Windows.Forms;
 
     internal static class Program
     {
-        internal static int RunAsAdministrator(string executablePath, string[] args)
-        {
-
-            var identity = WindowsIdentity.GetCurrent();
-            if (new WindowsPrincipal(identity).IsInRole(WindowsBuiltInRole.Administrator))
-            {
-                return 1;
-            }
-            const string key = "--SELFCALL";
-            if (args != null && args.Any(i => i == key))
-            {
-                return -1;
-            }
-            else
-            {
-                //创建启动对象
-                //如果不是管理员，则启动UAC
-                var startInfo = new ProcessStartInfo
-                {  //设置运行文件
-                    FileName = executablePath,
-                    //设置启动参数
-                    Arguments = string.Join(" ", args) + " " + key,
-                    //设置启动动作,确保以管理员身份运行
-                    Verb = "runas"
-                };
-                try
-                {
-                    Process.Start(startInfo);
-                }
-                catch (System.ComponentModel.Win32Exception)
-                {
-                    return -1;
-                }
-
-                //退出
-                return 0;
-            }
-
-
-
-        }
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        private static void Main(string[] args)
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             // TODO: set encryption support
             // ProtectedConfigurationProvider.Provider = new WorkingEncryptionServiceProvider();
-            var code = RunAsAdministrator(Application.ExecutablePath, args);
-            if (code > 0)
-                Application.Run(new MainForm());
-            else if (code < 0)
-                MessageBox.Show("Please using an account with role of Administrator login windows, and try again.");
+
+            Application.Run(new MainForm());
         }
     }
-
-
-
 }
